0a30e6656eda5514942ff07679e70a63f507241a
sclark57 <95256672+sclark57@users.noreply.github.com>
2022-12-20 21:34:11 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -20,15 +20,9 @@ if __name__ == "__main__":
 		.getOrCreate()
 	
 	#Read and load census data (training and testing)
-	df_train = spark.read.load('../input/adult_train.csv', 
-		format='com.databricks.spark.csv', 
-        header='false', 
-        inferSchema='true').cache()
+	df_train = spark.read.load('../input/adult_train.csv', format='com.databricks.spark.csv', header='false', inferSchema='true').cache()
 	
-	df_test = spark.read.load('../input/adult_test.csv', 
-		format='com.databricks.spark.csv', 
-        header='false', 
-        inferSchema='true').cache()
+	df_test = spark.read.load('../input/adult_test.csv', format='com.databricks.spark.csv', header='false', inferSchema='true').cache()
 
 	#Rename and clean up columns
 	education_num = col("`education.num`")
@@ -41,9 +35,9 @@ if __name__ == "__main__":
 	df_train = df.withColumn("education_num", education_num).drop(education_num)\
    		.withColumn("capital_gain", capital_gain).drop(capital_gain)\
    		.withColumn("capital_loss", capital_loss).drop(capital_loss)\
-    	.withColumn("hours_per_week", hours_per_week).drop(hours_per_week)\
-    	.withColumn("marital_status", marital_status).drop(marital_status)\
-    	.withColumn("native_country", native_country).drop(native_country)
+    		.withColumn("hours_per_week", hours_per_week).drop(hours_per_week)\
+    		.withColumn("marital_status", marital_status).drop(marital_status)\
+    		.withColumn("native_country", native_country).drop(native_country)
     
 	#Sort columns into numerical and categorical types
 	num_cols = ["age", "fnlwgt", "education_num", "capital_gain", "capital_loss", "hours_per_week"]
@@ -53,27 +47,27 @@ if __name__ == "__main__":
 	#Featurize categorical columns
 	stages = []
 	for col in cat_cols:
-    	#Category Indexing with StringIndexer
-    	stringIndexer = StringIndexer(
-       		inputCol=col, 
-        	outputCol=col+"_indx")
-    	#OneHotEncoder to convert categorical variables into binary SparseVectors
-    	encoder = OneHotEncoder(
-       		inputCols=[stringIndexer.getOutputCol()], 
-        	outputCols=[col + "_vec"])
+    		#Category Indexing with StringIndexer
+    		stringIndexer = StringIndexer(
+       			inputCol=col, 
+        		outputCol=col+"_indx")
+    		#OneHotEncoder to convert categorical variables into binary SparseVectors
+    		encoder = OneHotEncoder(
+       			inputCols=[stringIndexer.getOutputCol()], 
+        		outputCols=[col + "_vec"])
    		#Add stages.  These are not run here, but will run all at once later on.
-    	stages += [stringIndexer, encoder]
+    		stages += [stringIndexer, encoder]
     
 	#Convert label into label indices using the StringIndexer
 	label_indx = StringIndexer(
-    	inputCol="income", 
+    		inputCol="income", 
    		outputCol="label")
 	stages += [label_indx]
 
 	assembler_inputs = [c+"_vec" for c in cat_cols] + num_cols
 	assembler = VectorAssembler(
-    	inputCols=assembler_inputs, 
-    	outputCol="features")
+    		inputCols=assembler_inputs, 
+    		outputCol="features")
 	stages += [assembler]
 
 	#Create the pipeline
@@ -98,7 +92,7 @@ if __name__ == "__main__":
 	
 	#Evaluate model
 	pred = logModel.transform(df_test)
-    evaluator = BinaryClassificationEvaluator(labelCol="label")
-    print(evaluator.evaluate(pred))
+   	evaluator = BinaryClassificationEvaluator(labelCol="label")
+   	print(evaluator.evaluate(pred))
 	
 	spark.stop()
