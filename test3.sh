8456ac55d1ca826903368dcc92073ee981469d29
sclark57 <95256672+sclark57@users.noreply.github.com>
2022-12-20 21:35:58 -0500

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -4,4 +4,4 @@ source ../../env.sh
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /project2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../project2/data/adult_test.csv /project2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../project2/data/adult_train.csv /project2/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./part3_1.py hdfs://$SPARK_MASTER:9000/project2/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./part3.py hdfs://$SPARK_MASTER:9000/project2/input/
