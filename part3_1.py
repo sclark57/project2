23510605a735ad39aa185e549f357f8599bb50b0
sclark57 <95256672+sclark57@users.noreply.github.com>
2022-12-20 19:59:11 -0500

    Update part3_1.py

--- part3_1.py
+++ part3_1.py
@@ -6,9 +6,9 @@ from __future__ import print_function
 import sys
 from subprocess import check_output
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count, col
+from pyspark.sql.functions import (count, col)
 from pyspark.ml import Pipeline
-from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
+from pyspark.ml.feature import (OneHotEncoderEstimator, StringIndexer, VectorAssembler)
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
